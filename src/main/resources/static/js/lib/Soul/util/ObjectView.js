51248594f1560e99590b5b429ab7a53e52cd8c0d
guodong <cody.guo@soulinfo.com>
2019-03-20 21:46:05 +0800

    案例导入导出

--- src/main/resources/static/js/lib/Soul/util/ObjectView.js
+++ src/main/resources/static/js/lib/Soul/util/ObjectView.js
@@ -93,6 +93,43 @@ Ext.define('Soul.util.ObjectView', {
 			modal: true
 		});
 		win.show();
-	}
+    },
+
+    showDownloadWin: function (downloadInfo) {
+
+        var infoGrid = Ext.create('Soul.view.PropertyGrid', {
+            width: 300,
+            propertyNames: {
+                "size": "文件大小",
+                "taskNum": "个数"
+            },
+            customRenderers: {
+                "size": Soul.util.RendererUtil.getCapacityStrFormBytes
+            },
+            properties: ["taskNum", "size"],
+            source: downloadInfo
+        });
+
+        var win = new Ext.Window({
+            title: "下载",
+            items: infoGrid,
+            stateful: false,
+            autoDestroy: true,
+            bodyStyle: 'padding:5px',
+            modal: true,
+            buttonAlign: 'center',
+            buttons: [{
+                text: "下载",
+                href: downloadInfo.url,
+                hrefTarget: '_blank'
+            }, {
+                text: LABEL.cancel,
+                handler: function () {
+                    win.close();
+                }
+            }]
+        });
+        win.show();
+    }
 	
 });
